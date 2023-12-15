7e22830fa21410a6cba04f0d9d76afb4aff319fb
DESKTOP-8B4QBQ3\Windows <neal.robben@gmail.com>
2023-12-15 21:10:54 +0100

    Fixed build warning

--- WebUI/Client/Components/ProfilePicture.razor.cs
+++ WebUI/Client/Components/ProfilePicture.razor.cs
@@ -4,26 +4,9 @@ using Microsoft.AspNetCore.Components;
 
 public partial class ProfilePicture
 {
-    private string _fileName;
+    private string Link => !string.IsNullOrWhiteSpace(FileName) ? string.Concat("api/File/", FileName) : emptyUrl;
     private const string emptyUrl = "img/person.png";
 
     [Parameter]
-    public string FileName
-    {
-        get
-        {
-            if (!string.IsNullOrWhiteSpace(_fileName))
-            {
-                return string.Concat("api/File/", _fileName);
-            }
-            else
-            {
-                return emptyUrl;
-            }
-        }
-        set
-        {
-            _fileName = value;
-        }
-    }
+    public string FileName { get; set; }
 }
