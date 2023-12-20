c683d90428c2a7032c3f3814f21c0c6c428f6da9
DESKTOP-8B4QBQ3\Windows <neal.robben@gmail.com>
2023-12-20 22:36:49 +0100

    Removed automapper

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -11,9 +11,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        //Removed because we use the one in WebUI.Shared and automapper only accepts one assembly
-        //services.AddAutoMapper(Assembly.GetExecutingAssembly()); //NOSONAR
-
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
