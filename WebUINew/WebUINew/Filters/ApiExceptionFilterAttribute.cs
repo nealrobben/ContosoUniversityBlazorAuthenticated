d58ec62478b62ff01db14e90905da30d2d48e9bf
CEGEKANV\nealr <neal.robben@cegeka.com>
2026-04-25 19:32:48 +0200

    Fix build

---
+++ WebUINew/WebUINew/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,89 @@
+﻿using Application.Common.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebUINew.Filters;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+{
+
+    private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+    public ApiExceptionFilterAttribute()
+    {
+        // Register known exception types and handlers.
+        _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
+        {
+            { typeof(ValidationException), HandleValidationException },
+            { typeof(NotFoundException), HandleNotFoundException },
+        };
+    }
+
+    public override void OnException(ExceptionContext context)
+    {
+        HandleException(context);
+
+        base.OnException(context);
+    }
+
+    private void HandleException(ExceptionContext context)
+    {
+        Type type = context.Exception.GetType();
+
+        if (_exceptionHandlers.TryGetValue(type, out Action<ExceptionContext> value))
+        {
+            value.Invoke(context);
+            return;
+        }
+
+        HandleUnknownException(context);
+    }
+
+    private static void HandleUnknownException(ExceptionContext context)
+    {
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "An error occurred while processing your request.",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+        };
+
+        context.Result = new ObjectResult(details)
+        {
+            StatusCode = StatusCodes.Status500InternalServerError
+        };
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleValidationException(ExceptionContext context)
+    {
+        var exception = context.Exception as ValidationException;
+
+        var details = new ValidationProblemDetails(exception.Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        context.Result = new BadRequestObjectResult(details);
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleNotFoundException(ExceptionContext context)
+    {
+        var exception = context.Exception as NotFoundException;
+
+        var details = new ProblemDetails()
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "The specified resource was not found.",
+            Detail = exception.Message
+        };
+
+        context.Result = new NotFoundObjectResult(details);
+
+        context.ExceptionHandled = true;
+    }
+}
