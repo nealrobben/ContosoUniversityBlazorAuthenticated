167e12d1d252ccd6621051b42492a26e19988e1a
CEGEKANV\nealr <neal.robben@cegeka.com>
2026-04-26 18:59:40 +0200

    Add DTOs for Home

---
+++ WebUINew/WebUINew.Client/Dtos/Home/AboutInfoDto.cs
@@ -0,0 +1,16 @@
+﻿namespace WebUINew.Client.Dtos.Home;
+
+public class AboutInfoDto
+{
+    public IEnumerable<EnrollmentDateGroupDto> Items { get; set; }
+
+    public AboutInfoDto()
+    {
+        Items = [];
+    }
+
+    public AboutInfoDto(IEnumerable<EnrollmentDateGroupDto> items)
+    {
+        Items = items;
+    }
+}
---
+++ WebUINew/WebUINew.Client/Dtos/Home/EnrollmentDateGroupDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebUINew.Client.Dtos.Home;
+
+public class EnrollmentDateGroupDto
+{
+    public DateTime? EnrollmentDate { get; set; }
+
+    public int StudentCount { get; set; }
+}
