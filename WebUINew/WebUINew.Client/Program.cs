db30f655e1d502274b8cb3bea16efafc0c72aaae
DESKTOP-8B4QBQ3\Windows <neal.robben@gmail.com>
2026-04-26 15:24:47 +0200

    Update WebUINew.Client

--- WebUINew/WebUINew.Client/Program.cs
+++ WebUINew/WebUINew.Client/Program.cs
@@ -1,13 +1,55 @@
+using Blazored.LocalStorage;
+using FluentValidation;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace WebUINew.Client;
 
-internal class Program
+[ExcludeFromCodeCoverage]
+internal static class Program
 {
     static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        await builder.Build().RunAsync();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddMudServices();
+
+        RegisterServices(builder);
+
+        builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Localization"; });
+
+        builder.Services.AddBlazoredLocalStorage();
+        //builder.Services.AddValidatorsFromAssemblyContaining<CreateCourseInputModel>(); //TODO
+
+        var host = builder.Build();
+
+        await SetCulture(host);
+
+        await host.RunAsync();
+    }
+
+    private static void RegisterServices(WebAssemblyHostBuilder builder)
+    {
+        //builder.Services.AddScoped<IDepartmentService, DepartmentService>();//TODO
+        //builder.Services.AddScoped<ICourseService, CourseService>();//TODO
+        //builder.Services.AddScoped<IInstructorService, InstructorService>();//TODO
+        //builder.Services.AddScoped<IStudentService, StudentService>();//TODO
+        //builder.Services.AddScoped<IFileUploadService, FileUploadService>();//TODO
+        //builder.Services.AddScoped<ClientSettingService>();//TODO
+    }
+
+    private static async Task SetCulture(WebAssemblyHost host)
+    {
+        //var clientSettingService = host.Services.GetRequiredService<ClientSettingService>(); //TODO
+        //if (clientSettingService != null)
+        //{
+        //    var preference = await clientSettingService.GetSettings();
+        //    var culture = preference != null ? new CultureInfo(preference.LanguageCode) : new CultureInfo("en-US");
+        //    CultureInfo.DefaultThreadCurrentCulture = culture;
+        //    CultureInfo.DefaultThreadCurrentUICulture = culture;
+        //}
     }
 }
